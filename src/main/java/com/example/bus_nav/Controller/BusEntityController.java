d9736e9a6e30e076d37c45b6806b414b05124536
Meettt275 <143588417+Meettt275@users.noreply.github.com>
2024-03-08 21:50:07 +0530

    Back-End

--- src/main/java/com/example/bus_nav/Controller/BusEntityController.java
+++ src/main/java/com/example/bus_nav/Controller/BusEntityController.java
@@ -1,4 +1,23 @@
 package com.example.bus_nav.Controller;
 
+import com.example.bus_nav.Service.BusEntityService;
+import com.example.bus_nav.entity.BusEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/manage")
+@CrossOrigin
 public class BusEntityController  {
+    @Autowired
+    private BusEntityService busEntityService;
+    @PostMapping("/add")
+    public BusEntity addBusDetails(@RequestBody BusEntity busEntity){
+        return busEntityService.saveBus(busEntity);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteBusEntity(@PathVariable long id){
+        busEntityService.deleteBus(id);
+    }
+
 }
